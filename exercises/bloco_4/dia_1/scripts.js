b9c46f119ffc783d93c5ec6574c5071358934e9f
Lucas Godoi <lucazgodoy1996@gmail.com>
2021-02-24 16:29:06 -0300

    Adicionando Exercicio 1 Bloco_4 dia_1 , Operacaoes aritmetica basica

--- exercises/bloco_4/dia_1/scripts.js
+++ exercises/bloco_4/dia_1/scripts.js
@@ -1,4 +1,24 @@
-let a = 20;
-let b = 50;
-let subtracao = a - b;
+let a = 5;
+let b = 8;
+let resultado = a + b 
+console.log(resultado)
+
+let c = 20;
+let d = 50;
+let subtracao = c - d;
 console.log(subtracao)
+
+let e = 50;
+let f = 10;
+let multiplicacao = e * f;
+console.log(multiplicacao)
+
+let g = 40 
+let h = 20 
+let divisao = g / h 
+console.log(divisao)
+
+let i = 3
+let j = 7
+let potencializacao = i ** j
+console.log (potencializacao)
