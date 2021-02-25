fb714485bcec30dda0970f7c2fe4ef3eb6d48322
Lucas Godoi <lucazgodoy1996@gmail.com>
2021-02-25 00:03:33 -0300

    Comentando exercicio 6

--- exercises/bloco_4/dia_1/scripts.js
+++ exercises/bloco_4/dia_1/scripts.js
@@ -67,20 +67,18 @@ let xadrez = "Rainha";
 
 switch (xadrez.toLowerCase()) {
     case "bispo":
-        console.log("se move em diagonal")
+        // console.log("se move em diagonal")
         break;
-
     case "rei":
-        console.log("se move na vertical e na horizontal")
+        // console.log("se move na vertical e na horizontal")
         break;
-
     case "torre" :
-        console.log("se move reto")
+        // console.log("se move reto")
         break;
     case "rainha" :
-        console.log("se move de todas as formas")
+        // console.log("se move de todas as formas")
         break;;
     case "pipa" :
-        console.log("erro")
+        // console.log("erro")
         break;
 }
