a82f3bc0a94500b24023f10b99444e9a2c57aa78
Lucas Godoi <lucazgodoy1996@gmail.com>
2021-02-24 23:06:07 -0300

    Exercicio 2 Bloco_4 dia1

--- exercises/bloco_4/dia_1/scripts.js
+++ exercises/bloco_4/dia_1/scripts.js
@@ -17,13 +17,11 @@ let modulo = a % b ;
 // console.log(modulo)
 
 // Exercicios 2
-let num1 = 10
-let num2 = 20
+let num1 = 10;
+let num2 = 20;
 
 if(10 > 20){
     // console.log( "menor")
 } else if ( 20 > 10){
     //  console.log("maior")
 }
-
-// Exercicios 3
