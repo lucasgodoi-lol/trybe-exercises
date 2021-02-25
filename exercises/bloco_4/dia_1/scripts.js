2f029ac405967d55f03b38288a8319b436276103
Lucas Godoi <lucazgodoy1996@gmail.com>
2021-02-24 22:57:23 -0300

    Exercicios bloco 4 dia 1

--- exercises/bloco_4/dia_1/scripts.js
+++ exercises/bloco_4/dia_1/scripts.js
@@ -1,24 +1,17 @@
 let a = 5;
 let b = 8;
-let resultado = a + b 
-console.log(resultado)
 
-let c = 20;
-let d = 50;
-let subtracao = c - d;
-console.log(subtracao)
+let soma = a + b ;
+console.log(soma)
 
-let e = 50;
-let f = 10;
-let multiplicacao = e * f;
-console.log(multiplicacao)
+let subtraçao = a - b;
+console.log(subtraçao)
 
-let g = 40 
-let h = 20 
-let divisao = g / h 
+let divisao =  a / b;
 console.log(divisao)
 
-let i = 3
-let j = 7
-let potencializacao = i ** j
-console.log (potencializacao)
+let multiplicacao = a * b;
+console.log(multiplicacao)
+
+let modulo = a % b ;
+console.log('modulo',modulo)
