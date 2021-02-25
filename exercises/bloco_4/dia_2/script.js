d6224f534202f8681728998775ea413e2fa51334
Lucas Godoi <lucazgodoy1996@gmail.com>
2021-02-25 15:42:39 -0300

    Exercicio 2 array

--- exercises/bloco_4/dia_2/script.js
+++ exercises/bloco_4/dia_2/script.js
@@ -3,4 +3,9 @@
 let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
 let menuServices = menu[1];
 
-console.log(menuServices);
+// console.log(menuServices);
+
+// Exercicio 2
+
+let indexOfPortfolio = menu.indexOf('Portfólio')
+console.log(indexOfPortfolio)
