84881f0c82598a745077cc422be8d761655a9197
Lucas Godoi <lucazgodoy1996@gmail.com>
2021-02-24 23:04:20 -0300

    Exercicio 2 Bloco_4 Dia_1

--- exercises/bloco_4/dia_1/scripts.js
+++ exercises/bloco_4/dia_1/scripts.js
@@ -2,16 +2,28 @@ let a = 5;
 let b = 8;
 
 let soma = a + b ;
-console.log(soma)
+// console.log(soma)
 
 let subtraçao = a - b;
-console.log(subtraçao)
+// console.log(subtraçao)
 
 let divisao =  a / b;
-console.log(divisao)
+// console.log(divisao)
 
 let multiplicacao = a * b;
-console.log(multiplicacao)
+// console.log(multiplicacao)
 
 let modulo = a % b ;
-console.log('modulo',modulo)
+// console.log(modulo)
+
+// Exercicios 2
+let num1 = 10
+let num2 = 20
+
+if(10 > 20){
+    // console.log( "menor")
+} else if ( 20 > 10){
+    //  console.log("maior")
+}
+
+// Exercicios 3
