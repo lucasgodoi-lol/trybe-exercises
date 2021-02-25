2ef1df33e5b2f0c881759f53516b3c7b3467f17d
Lucas Godoi <lucazgodoy1996@gmail.com>
2021-02-25 00:25:06 -0300

    Exercicio 8 Bloco4 dia 1

--- exercises/bloco_4/dia_1/scripts.js
+++ exercises/bloco_4/dia_1/scripts.js
@@ -87,19 +87,30 @@ switch (xadrez.toLowerCase()) {
 let nota = 95
 
 if (nota >= 90 ){
-    console.log ("A")
+    // console.log ("A")
 }
 else if (nota >= 80){
-    console.log ("B")
+    // console.log ("B")
 }
 else if (nota >= 70){
-    console.log("C")
+    // console.log("C")
 }else if (nota >= 60){
-    console.log("D")
+    // console.log("D")
 }else if (nota >=50){
-    console.log("E")
+    // console.log("E")
 }else if (nota < 50){
-    console.log("F")
+    // console.log("F")
 }else if (nota < 0 || nota > 100){
-    console.log ("Erro")
+    // console.log ("Erro")
+}
+
+// Exercicio 8 
+a = 5
+b = 3
+c = 7
+
+if (a % 2 === 0 || b % 2 === 0 || c % 2 === 0 ){
+    console.log(true)
+}else  {
+    console.log(false)
 }
