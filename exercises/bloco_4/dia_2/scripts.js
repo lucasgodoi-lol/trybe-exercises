75e2890712b0a121a533a38e4773e00496505a88
Lucas Godoi <lucazgodoy1996@gmail.com>
2021-02-26 09:26:02 -0300

    Exercicio 4 Bloco_4 Dia 2

--- exercises/bloco_4/dia_2/scripts.js
+++ exercises/bloco_4/dia_2/scripts.js
@@ -26,3 +26,18 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 //    valor = valor + numbers[index] / 10 ;
 // }
 // console.log(valor.toFixed(2))
+
+// Exercicio 4
+
+// let valor = 0; 
+
+// for(let index = 0; index < numbers.length;index +=1){
+//     valor = valor + numbers[index] / 10 ;
+// }
+
+//  if(valor > 20 ){
+//     console.log("Valor maior que 20")
+// }else {
+//     console.log("Valor menor que 20")
+//     }
+
