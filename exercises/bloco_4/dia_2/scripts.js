bb3c2794c1b9e2235f94dfa61dc3850aae600c14
Lucas Godoi <lucazgodoy1996@gmail.com>
2021-02-26 09:13:19 -0300

    Adicionando console.logg fora do loop exercicio2

--- exercises/bloco_4/dia_2/scripts.js
+++ exercises/bloco_4/dia_2/scripts.js
@@ -14,6 +14,7 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27]
 let valor = 0;
 
 for(let index = 0;index < numbers.length;index += 1){
-   valor = valor + numbers[index]
-//    console.log(valor)
+    valor +=numbers[index] 
 }
+console.log(valor)
+
