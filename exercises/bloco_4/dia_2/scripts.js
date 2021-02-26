ef88acc2aa69b0fb1788d4ca44de0b9211a0ec9e
Lucas Godoi <lucazgodoy1996@gmail.com>
2021-02-26 09:45:28 -0300

    comentando

--- exercises/bloco_4/dia_2/scripts.js
+++ exercises/bloco_4/dia_2/scripts.js
@@ -43,10 +43,12 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 // Exercicio 5
 
-let numeromaior = 0;
+// let numeromaior = 0;
 
-for( let index =0;index  < numbers.length;index +=1){
-    if(numbers[index] > numeromaior){
-    numeromaior = numbers[index]
-    }
-}console.log("O numero maior é = " + numeromaior)
+// for( let index =0;index  < numbers.length;index +=1){
+//     if(numbers[index] > numeromaior){
+//     numeromaior = numbers[index]
+//     }
+// }console.log("O numero maior é = " + numeromaior)
+
+let
