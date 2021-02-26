4a8085f1498d9c41192ddf394e0734fae51ff03e
Lucas Godoi <lucazgodoy1996@gmail.com>
2021-02-26 09:21:35 -0300

    Comentando Exercicio 3 bloco 4 dia 1

--- exercises/bloco_4/dia_2/scripts.js
+++ exercises/bloco_4/dia_2/scripts.js
@@ -20,9 +20,9 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 // Exercicio 3
 
-let valor = 0 ;
+// let valor = 0 ;
 
-for(let index = 0; index < numbers.length;index +=1){
-   valor = valor + numbers[index] / 10 ;
-}
-console.log(valor.toFixed(2))
+// for(let index = 0; index < numbers.length;index +=1){
+//    valor = valor + numbers[index] / 10 ;
+// }
+// console.log(valor.toFixed(2))
