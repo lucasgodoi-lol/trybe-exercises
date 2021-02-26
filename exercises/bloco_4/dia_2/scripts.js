12bdc31d46dac5f4cf87027a8e88ba28652ed974
Lucas Godoi <lucazgodoy1996@gmail.com>
2021-02-26 09:20:43 -0300

    Adicionando Exercicio 3 Bloco_4 Dia_1

--- exercises/bloco_4/dia_2/scripts.js
+++ exercises/bloco_4/dia_2/scripts.js
@@ -1,20 +1,28 @@
 // Exercicio 1
-// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 // for(let index = 0;index < numbers.length;index += 1){
-    // console.log(numbers[index])
+//     console.log(numbers[index])
 // }
 
 // console.log(numbers)
 
 // Exercicio 2
 
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27]
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27]
 
-let valor = 0;
+// let valor = 0;
 
-for(let index = 0;index < numbers.length;index += 1){
-    valor +=numbers[index] 
+// for(let index = 0;index < numbers.length;index += 1){
+//     valor = valor + numbers[index] 
+// }
+// console.log(valor)
+
+// Exercicio 3
+
+let valor = 0 ;
+
+for(let index = 0; index < numbers.length;index +=1){
+   valor = valor + numbers[index] / 10 ;
 }
-console.log(valor)
-
+console.log(valor.toFixed(2))
