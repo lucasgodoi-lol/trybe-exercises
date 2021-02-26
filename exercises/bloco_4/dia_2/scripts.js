abb9495a974d57321a4b2a527913b7985da08f34
Lucas Godoi <lucazgodoy1996@gmail.com>
2021-02-26 09:03:49 -0300

    Exercicio 2 Bloco_4 Dia 1

---
+++ exercises/bloco_4/dia_2/scripts.js
@@ -0,0 +1,19 @@
+// Exercicio 1
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// for(let index = 0;index < numbers.length;index += 1){
+    // console.log(numbers[index])
+// }
+
+// console.log(numbers)
+
+// Exercicio 2
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27]
+
+let valor = 0;
+
+for(let index = 0;index < numbers.length;index += 1){
+   valor = valor + numbers[index]
+//    console.log(valor)
+}
