4e15c73d776efc22fa6a9e5b9758fdb11a61e551
Lucas Godoi <lucazgodoy1996@gmail.com>
2021-03-01 08:07:43 -0300

    Exercice two (Make a program that print a triangle rectangle

--- exercises/bloco_4/dia_3/scripts.js
+++ exercises/bloco_4/dia_3/scripts.js
@@ -10,3 +10,16 @@ for(let index = 0;index < n;index ++){
 for(let index = 0;index < n ; index ++){
     // console.log(line)
 }
+
+
+
+n = 5;
+asteristik = "*"
+line = ""
+
+// Exercice two ;
+
+for(let index = 0;index < n;index ++){
+    line = line + asteristik
+    console.log(line)
+}
