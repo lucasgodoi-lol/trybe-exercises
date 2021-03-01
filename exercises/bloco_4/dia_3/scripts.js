9a23d4de4b482a3d7903efde47b596763115d230
Lucas Godoi <lucazgodoy1996@gmail.com>
2021-03-01 08:04:27 -0300

    Exercice 1 (Make one program that print on screen a square done asteristik

---
+++ exercises/bloco_4/dia_3/scripts.js
@@ -0,0 +1,12 @@
+// Exercice  One ;
+
+let n = 5;
+let asteristik = "*"
+let line = ""
+
+for(let index = 0;index < n;index ++){
+    line = line + asteristik
+}
+for(let index = 0;index < n ; index ++){
+    // console.log(line)
+}
