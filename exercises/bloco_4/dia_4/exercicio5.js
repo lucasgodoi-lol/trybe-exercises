07c140cf316662240253ad40c43cd92be75319f2
Lucas Godoi <lucazgodoy1996@gmail.com>
2021-03-02 23:26:38 -0300

    exercicio 5,4.4

--- exercises/bloco_4/dia_4/exercicio5.js
+++ exercises/bloco_4/dia_4/exercicio5.js
@@ -1,29 +1,27 @@
-//Exercicio 5
-let info = {
+let info2 ={
+    personagem: "Tio Patinhas",
+    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: "O último MacPatinhas",
+    recorrente: 'Sim',
+  };
+
+
+
+  let info = {
     personagem: "Margarida",
     origem: "Pato Donald",
     nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
-    recorrente: "Sim"
+    recorrente: 'Sim'
 };
 
-
-
-let info2 = {
-    personagem: "Tio Patinha",
-    origem: "Christmas on Bear Mountain,Dell's Four Color Comics #178",
-    nota: "O último MacPatinhas",
-    recorrente: "Sim"
-}
-
-for (let key in info) {
-    for(let key2 in info2){
-    if(info[key] !== info2[key2] && key === key2){
-    console.log(info[key] +  " e " + info2[key2])
-    }
- }
-}
- if (info['recorrente'] === info2['recorrente']) {
+  for(key in info){
+      for(key2 in info2)
+     if(key === key2 && info[key] !== info2[key2]){
+         console.log(info[key] + " " + "e " + info2[key2])
+      
+     }
+    
+} if(info.recorrente === info2.recorrente){
     console.log("Ambos recorrentes")
-} 
 
-
+}
