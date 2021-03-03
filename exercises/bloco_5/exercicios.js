c8e004d9088753017112a01e53638b71dbb9587f
Lucas Godoi <lucazgodoy1996@gmail.com>
2021-03-03 15:30:58 -0300

    finalizando primeiro exercicio dos videos bloco_5 dia 1

---
+++ exercises/bloco_5/exercicios.js
@@ -0,0 +1,6 @@
+var paragraph = document.getElementById("paragraph");
+paragraph.style.color = "red";
+document.getElementById("page-title").innerText = "Quebrando Regras";
+document.getElementById("paragraph").innerText = "Esse filme nos mostra para nunca desistirmos dos nossos objetivos, com muita dedicação chegaremos la !!"
+document.getElementById("subtitle").innerText = "Recomendo a todos"
+document.getElementById("second-paragraph").innerText = "Se nunca assistiu assista ja !"
