4c06f38739d572c70dbc8eee6c4d6b94e6bbf307
Lucas Godoi <lucazgodoy1996@gmail.com>
2021-03-03 15:53:18 -0300

    Exercicio 2videos

--- exercises/bloco_5/exercicios.js
+++ exercises/bloco_5/exercicios.js
@@ -2,5 +2,6 @@ var paragraph = document.getElementById("paragraph");
 paragraph.style.color = "red";
 document.getElementById("page-title").innerText = "Quebrando Regras";
 document.getElementById("paragraph").innerText = "Esse filme nos mostra para nunca desistirmos dos nossos objetivos, com muita dedicação chegaremos la !!"
-document.getElementById("subtitle").innerText = "Recomendo a todos"
 document.getElementById("second-paragraph").innerText = "Se nunca assistiu assista ja !"
+let styleDosP = document.getElementsByClassName("style")[0].style.color = "blue"
+document.getElementsByTagName("h4")[0].innerText = "Não perca !"
