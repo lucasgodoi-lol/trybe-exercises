1d37dfdfc8a4e746938b7325ad96159febb968fe
Lucas Godoi <lucazgodoy1996@gmail.com>
2021-03-03 22:50:50 -0300

    Exercicio 1

---
+++ exercises/bloco_5/dia_1/exercicios/script.js
@@ -0,0 +1,5 @@
+//  Exercicio 1
+function paragrafo(p){
+    return document.getElementsByTagName("p")[0].innerText = p
+  }
+  paragrafo("Daqui a dois anos eu espero estar empregado em uma boa empresa, e podendo viajar com minha familia.")
