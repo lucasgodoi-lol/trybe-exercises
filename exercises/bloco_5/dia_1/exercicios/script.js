98e5058a4f0fe6ee57ec41c2d0abec48bd495149
Lucas Godoi <lucazgodoy1996@gmail.com>
2021-03-04 00:06:44 -0300

    Exercicio 3 e 4

--- exercises/bloco_5/dia_1/exercicios/script.js
+++ exercises/bloco_5/dia_1/exercicios/script.js
@@ -1,8 +1,9 @@
 //  Exercicio 1
-function paragrafo(p){
+function paragrafo(p,p2){
   document.getElementsByTagName("p")[0].innerText = p
+  document.getElementsByTagName("p")[1].innerText = p2
   }
-  paragrafo("Daqui a dois anos eu espero estar empregado em uma boa empresa, e podendo viajar com minha familia.")
+  paragrafo("Daqui a dois anos eu espero estar empregado em uma boa empresa, e podendo viajar com minha familia.","Aprender inglês e varias linguagens de programação")
 
   
 //   Exercicio 2 
@@ -10,6 +11,19 @@ function corQuadrado(color){
     document.getElementsByClassName('main-content')[0].style.backgroundColor = color
    }
 corQuadrado("rgb(76,164,109)")
+
+// Exercicio 3 ;
+function redSquare(color1){
+    document.getElementsByClassName('center-content')[0].style.backgroundColor = color1;
+}
+redSquare("white");
+
+// Exercicio 4 ;
+
+function errorText(text){
+    document.getElementsByClassName('title')[0].innerText = text;
+}
+errorText("Exercício 5.1 - JavaScript")
 
 
 // function  backGround(color){
