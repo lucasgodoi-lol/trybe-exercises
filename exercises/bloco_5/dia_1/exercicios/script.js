061fbe74a067477c4fcbcc29270af8daaf0b6dbf
Lucas Godoi <lucazgodoy1996@gmail.com>
2021-03-03 22:35:07 -0300

    Apagando arquivos

--- exercises/bloco_5/dia_1/exercicios/script.js
+++
@@ -1,9 +0,0 @@
-let a = document.getElementsByClassName("center-content")
-
-function about2Years(eu){
-for(let index = 0;index < a.length;index +=1){
-    a[index].innerHTML = eu
-}
-return eu ;
-}
-about2Years("Daqui a 2 ano me imagino trabalhando em uma empresa boa , e tendo uma estabilidade financeira ,podendo viajar e fazer coisas para mim e minha familia")
