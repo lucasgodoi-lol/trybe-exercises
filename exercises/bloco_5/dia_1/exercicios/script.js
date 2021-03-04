0e913f52bc0da2d65b095a6152dcb4947715fb17
Lucas Godoi <lucazgodoy1996@gmail.com>
2021-03-03 23:55:58 -0300

    Exercicio 2

--- exercises/bloco_5/dia_1/exercicios/script.js
+++ exercises/bloco_5/dia_1/exercicios/script.js
@@ -1,5 +1,17 @@
 //  Exercicio 1
 function paragrafo(p){
-    return document.getElementsByTagName("p")[0].innerText = p
+  document.getElementsByTagName("p")[0].innerText = p
   }
   paragrafo("Daqui a dois anos eu espero estar empregado em uma boa empresa, e podendo viajar com minha familia.")
+
+  
+//   Exercicio 2 
+function corQuadrado(color){
+    document.getElementsByClassName('main-content')[0].style.backgroundColor = color
+   }
+corQuadrado("rgb(76,164,109)")
+
+
+// function  backGround(color){
+//     let color1 = document.getElementsByClassName("main-content")[0].style
+// }
