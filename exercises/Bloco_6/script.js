0977487b91eaf3000beb443931f9bc759bab9b3e
Lucas Godoi <lucazgodoy1996@gmail.com>
2021-03-17 03:55:19 -0300

    Deixando maneirinho o form

--- exercises/Bloco_6/script.js
+++ exercises/Bloco_6/script.js
@@ -1,11 +1,11 @@
-function statesOfBrazil(){
-    const arrayStates = ['Selecione um estado', 'Acre','Alagoas','Amapá','Amazonas','Bahia','Ceará','Distrito Federal','Espírito Santo','Goiás','Maranhão','Mato Grosso','Mato Grosso do Sul','Minas Gerais','Pará','Paraíba','Paraná','Pernambuco','Piauí','Rio de Janeiro','Rio Grande do Norte','Rio Grande do Sul','Rondônia','Roraima','Santa Catarina','São Paulo','Sergipe','Tocantins']
-   for(let index = 0; index < arrayStates.length; index += 1){
-       const states = document.createElement('option');
+function statesOfBrazil() {
+    const arrayStates = ['Selecione um estado', 'Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins']
+    for (let index = 0; index < arrayStates.length; index += 1) {
+        const states = document.createElement('option');
         const pai = document.getElementById('input-estados')
         pai.appendChild(states)
-       states.innerHTML = arrayStates[index]
-   }
+        states.innerHTML = arrayStates[index]
+    }
 }
 statesOfBrazil();
 
@@ -25,8 +25,88 @@ statesOfBrazil();
 //     })
 // }
 // data();
+function data(maxYear) {
+    const picker = new Pikaday({
+        field: document.getElementById('input-data'),
+        format: 'DD/MM/YYYY',
+        minYear: 2000,
+        maxYear: maxYear
+    });
+}
+data(2030);
 
-var picker = new Pikaday({ 
-    field: document.getElementById('input-data'),
-    format : 'DD/MM/YYYY',
-});
+function colorInput() {
+    const input = document.querySelectorAll('input')
+    for (let index = 0; index < input.length; index += 1) {
+        input[index].style.color = 'black'
+        input[index].style.fontWeight = 900;
+        input[index].classList.add('color')
+    }
+}
+colorInput();
+
+
+
+function click() {
+    const form = document.querySelector('form')
+    form.addEventListener('click', (e) => {
+        const target1 = e.target
+        const color = document.getElementsByClassName('color')
+        for (let index = 0; index < color.length; index += 1) {
+            color[index].style.backgroundColor = 'white'
+            if (target1.localName === 'input') {
+                target1.style.backgroundColor = 'LavenderBlush'
+            }
+        }
+    })
+}
+click();
+
+function colors() {
+    const option = document.querySelectorAll('option');
+    for (let index = 0; index < option.length; index += 1) {
+        const r = Math.floor(Math.random() * 255)
+        const g = Math.floor(Math.random() * 255)
+        const b = Math.floor(Math.random() * 255)
+        const rgb = `rgb(${r}, ${g}, ${b})`
+        if (rgb !== 'rgb(255,255,255)') {
+            option[index].style.backgroundColor = rgb;
+        }
+        option[index].style.color = 'white'
+        option[index].style.fontWeight = 900;
+    }
+}
+colors();
+
+
+function colorsOption() {
+    const select = document.querySelector('select');
+    select.addEventListener('click', () => {
+        const option = document.querySelectorAll('option');
+        for (let index = 0; index < option.length; index += 1) {
+            const r = Math.floor(Math.random() * 255)
+            const g = Math.floor(Math.random() * 255)
+            const b = Math.floor(Math.random() * 255)
+            const rgb = `rgb(${r}, ${g}, ${b})`
+            if (rgb !== 'rgb(255,255,255)') {
+                option[index].style.backgroundColor = rgb;
+            }
+        }
+    })
+}
+colorsOption();
+
+function click2() {
+    const textarea = document.getElementById('text-ultimoEmprego').style.fontWeight = 900;
+    const form = document.querySelector('form')
+    form.addEventListener('click', (e) => {
+        const target1 = e.target
+        const textarea = document.getElementById('text-ultimoEmprego')
+        textarea.style.backgroundColor = 'white'
+        if (target1.localName === 'textarea') {
+            target1.style.backgroundColor = 'LavenderBlush'
+        }
+    })
+}
+click2();
+
