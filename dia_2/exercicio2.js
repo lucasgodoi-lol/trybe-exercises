7f632c5e123f3624fd16ea0bfd655e76bfc9573e
Lucas Godoi <lucazgodoy1996@gmail.com>
2021-03-24 19:25:18 -0300

    feat(exercise 5 part 2): :fire: group objects

--- dia_2/exercicio2.js
+++ dia_2/exercicio2.js
@@ -18,44 +18,58 @@ const lesson1 = {
     turno: 'noite',
   };
 
-// Exercicio parte 2 
-// Exercicio 1 
+// // Exercicio parte 2 
+// // Exercicio 1 
 
-const addTurn = (object, key, value) => {
-    object[key] = value;
-    return object
-}
-console.log('Exercicio 1')
-console.log(addTurn(lesson2, 'turno', 'manhã'))
+// const addTurn = (object, key, value) => {
+//     object[key] = value;
+//     return object
+// }
+// console.log('Exercicio 1')
+// console.log(addTurn(lesson2, 'turno', 'manhã'))
 
-// Exercicio 2
+// // Exercicio 2
 
-const showTheKeys = (object) => {
-    const keys = Object.keys(object)
-    return keys
-}
-console.log('Exercicio 2')
-console.log(showTheKeys(lesson1))
+// const showTheKeys = (object) => {
+//     const keys = Object.keys(object)
+//     return keys
+// }
+// console.log('Exercicio 2')
+// console.log(showTheKeys(lesson1))
 
-// Exercicio 3
+// // Exercicio 3
 
-const sizeOfObject = (object1,object2, object3) => {
-    const size1 = Object.entries(object1);
-    const size2 = Object.entries(object2);
-    const size3 = Object.entries(object3);
-    return `O tamanho do primeiro Objeto é ${size1.length}, do segundo: ${size2.length}, do terceiro ${size3.length}`; 
-}
-console.log('Exercicio 3')
-console.log(sizeOfObject(lesson1, lesson2, lesson3));
+// const sizeOfObject = (object1,object2, object3) => {
+//     const size1 = Object.entries(object1);
+//     const size2 = Object.entries(object2);
+//     const size3 = Object.entries(object3);
+//     return `O tamanho do primeiro Objeto é ${size1.length}, do segundo: ${size2.length}, do terceiro ${size3.length}`; 
+// }
+// console.log('Exercicio 3')
+// console.log(sizeOfObject(lesson1, lesson2, lesson3));
 
-// Exercicio 4
+// // Exercicio 4
 
-const valuesOfObject = (object) => {
-    const valueOfObject = Object.values(object);
-    return valueOfObject;
-}
-console.log('Exercicio 4')
-console.log(valuesOfObject(lesson1));
+// const valuesOfObject = (object) => {
+//     const valueOfObject = Object.values(object);
+//     return valueOfObject;
+// }
+// console.log('Exercicio 4')
+// console.log(valuesOfObject(lesson1));
 
 // Exercicio 5
+ const allLesson = Object.assign({},{lesson1, lesson2, lesson3})
+ Object.assign(allLesson,{lesson2})
+ Object.assign(allLesson,{lesson3})
+ console.log(allLesson)
 
+const createNewObject = (lesson1, lesson2, lesson3) => {
+
+    const  allLessons = [lesson1, lesson2, lesson3];
+    let outPut = {};
+    for(let index = 0; index < allLessons.length; index += 1){
+     outPut[`lesson${index +1}`] = allLessons[index]
+    }
+    return  outPut;
+}
+console.log(createNewObject(lesson1, lesson2, lesson3));
