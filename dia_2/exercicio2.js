af56fe6121582ac75e421b4c8a5dcf5b8e11dc38
Lucas Godoi <lucazgodoy1996@gmail.com>
2021-03-24 18:36:59 -0300

    feat(Exercise 3 part 2): :zap: length of Object

--- dia_2/exercicio2.js
+++ dia_2/exercicio2.js
@@ -25,6 +25,25 @@ const addTurn = (object, keys, value) => {
     object[keys] = value;
     return object
 }
+console.log('Exercicio 1')
 console.log(addTurn(lesson2, 'turno', 'manhã'))
+
+// Exercicio 2
+
+const showTheKeys = (object) => {
+    const keys = Object.keys(object)
+    return keys
+}
+console.log('Exercicio 2')
+console.log(showTheKeys(lesson1))
+
+// Exercicio 3
+
+const sizeOfObject = (object) => {
+    const size = Object.entries(object);
+    return size.length;
+}
+console.log('Exercicio 3')
+console.log(sizeOfObject(lesson3));
 
 
