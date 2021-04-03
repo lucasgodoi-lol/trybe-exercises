8b2e6fc210cedbc782ef7050885b1e1dde25b532
Lucas Godoi <lucazgodoy1996@gmail.com>
2021-04-03 05:06:40 -0300

    fix(exercise 2): :fire:

--- exercises/Bloco_7/dia_3/script.js
+++ exercises/Bloco_7/dia_3/script.js
@@ -9,8 +9,11 @@ const contNumber = (number) =>  {
 }
 
 
-assert.strictEqual(typeof(contNumber),  'function', 'Tem que ser uma função chamada contNumber');
+assert.strictEqual(typeof(contNumber),  'function');
 assert.strictEqual(contNumber(5), 15)
 assert.strictEqual(contNumber(3), 6)
 assert.strictEqual(contNumber(1), 1)
 assert.strictEqual(contNumber(2), 3)
+
+const assert = require('assert');
+
