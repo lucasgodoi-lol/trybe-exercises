d0f55a7e899be3f4b8cd836059e0e1d3412ba4c7
Lucas Godoi <lucazgodoy1996@gmail.com>
2021-04-03 06:21:45 -0300

    feat(exercise 6 part1): :art:

--- exercises/Bloco_7/dia_3/a.js
+++ exercises/Bloco_7/dia_3/a.js
@@ -1,3 +1,8 @@
-const c = ['banana','maca','pera']
-c.splice(0,1, "maçã")
+
+
+let a = [1,3,6,7]
+let c =[]
+for(let index = 0; index < a.length; index ++) {
+  c.push(a[index] += 1)
+}
 console.log(c)
