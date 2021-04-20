d8e1ec82a9e2793961aad65f15ecd8c1fc80273d
Lucas Godoi <lucazgodoy1996@gmail.com>
2021-04-20 18:24:51 -0300

    exercise 1

--- Exercises/Módulo Fundamentos/Bloco_9/dia_2/a.js
+++ Exercises/Módulo Fundamentos/Bloco_9/dia_2/a.js
@@ -1,28 +1,6 @@
-const API_URL = 'https://icanhazdadjoke.csom/';
-
-const fetchJoke = async () => {
-    const myObject = {
-        method: 'GET',
-        headers: { 'Accept': 'application/json' }
-    };
-    const a = new TypeError('erro')
-    try {
-        const response = await fetch(API_URL, myObject);
-        const data = await response.json()
-        joke(data.joke)
-    }
-    catch(erro) {
-        joke(erro)
-    }
-
-    // .then(response => response.json())
-    // .then(data => {
-    //   joke(data)
-    // });
-};
-const joke = (text) => {
-    const h2 = document.querySelector('h2')
-    h2.innerHTML = text
-}
-
-window.onload = () => fetchJoke();
+test("Não deveria passar!", () => {
+    setTimeout(() => {
+      expect(10).toBe(5);
+      console.log('Deveria falhar!')
+    }, 500);
+  });
