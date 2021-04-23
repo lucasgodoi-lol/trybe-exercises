b56d3d502fb848e09349c25a1b8a9128c3d7d598
Lucas Godoi <lucazgodoy1996@gmail.com>
2021-04-22 22:51:41 -0300

    Exercise 4

--- Exercises/Módulo Fundamentos/Bloco_10/dia_3/src/exercise1,2,3.js
+++ Exercises/Módulo Fundamentos/Bloco_10/dia_3/src/exercise1,2,3.js
@@ -2,4 +2,18 @@ const randomNumber = () => {
     return Math.round(Math.random() * 100)
 }
 
-module.exports = { randomNumber }
+
+const showString = (str) => {
+  return str.toUpperCase();
+}
+
+const showFirstLetter = (str) => {
+    return str[0];
+}
+
+const concatStrings = (str1, str2) => {
+    return `${str1}${str2}`
+} 
+
+
+module.exports = { randomNumber, showFirstLetter , showString , concatStrings }
--- Exercises/Módulo Fundamentos/Bloco_10/dia_3/test/exercise1,2,3.test.js
+++ Exercises/Módulo Fundamentos/Bloco_10/dia_3/test/exercise1,2,3.test.js
@@ -1,18 +1,18 @@
 const result = require('../src/exercise1,2,3')
 
 test('test return of function randomNumber', () => {
-   result.randomNumber = jest 
-   .fn()
-   .mockReturnValue(10);
+   result.randomNumber = jest
+      .fn()
+      .mockReturnValue(10);
    expect(result.randomNumber()).toBe(10);
    expect(result.randomNumber).toHaveBeenCalled();
    expect(result.randomNumber).toHaveBeenCalledTimes(1)
 })
 
-test('test division of function randomNumber',() => {
+test('test division of function randomNumber', () => {
    result.randomNumber = jest
-   .fn()
-   .mockImplementation((a , b) => a / b);
+      .fn()
+      .mockImplementation((a, b) => a / b);
    expect(result.randomNumber(15, 3)).toBe(5);
    expect(result.randomNumber).toHaveBeenCalledTimes(1);
    expect(result.randomNumber).toHaveBeenLastCalledWith(15, 3)
@@ -20,13 +20,35 @@ test('test division of function randomNumber',() => {
 
 test('test implementation randomNumber', () => {
    const spyRandom = jest
-     .spyOn(result, 'randomNumber')
-     .mockImplementation((a, b, c) => a * b * c);
+      .spyOn(result, 'randomNumber')
+      .mockImplementation((a, b, c) => a * b * c);
    expect(spyRandom(2, 2, 3)).toBe(12);
    result.randomNumber.mockReset();
    result.randomNumber = jest
-     .fn()
-     .mockImplementation((a) => a * 2);
+      .fn()
+      .mockImplementation((a) => a * 2);
    expect(result.randomNumber(100)).toBe(200);
- });
+});
+
+
+describe('test functions showString , showFirstLetter and concatStrings', () => {
+   test('test function showString', () => {
+      result.showString = jest
+         .fn()
+         .mockImplementation((str) => str.toLowerCase());
+      expect(result.showString('AMANHÃ')).toBe('amanhã');
+   })
+   test('test function showFirstLetter', () => {
+      result.showFirstLetter = jest
+         .fn()
+         .mockImplementation((str) => str[str.length - 1]);
+      expect(result.showFirstLetter('Lucas')).toBe('s')
+   })
+   test('test function concatStrings', () => {
+      result.showFirstLetter = jest
+         .fn()
+         .mockImplementation((str1, str2, str3) => `${str1}${str2}${str3}`);
+      expect(result.showFirstLetter('Car', 'li', 'tos')).toBe('Carlitos')
+   })
+})
 
